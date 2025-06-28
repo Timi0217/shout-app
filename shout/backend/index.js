a2db8c2e79b69c01e1c6d0f803288912449b6bdb
Timi <timmydayokayode@gmail.com>
2025-06-28 15:51:47 -0400

    Update API_URL to live Railway backend

--- shout/backend/index.js
+++ shout/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
   origin: 'https://joinshout.fyi',
