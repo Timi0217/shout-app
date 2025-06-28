7f63c4f8810ad1ab158e83ea631412312893ac1f
Timi <timmydayokayode@gmail.com>
2025-06-28 17:22:22 -0400

    more cors issues trying to fix

--- shout/backend/index.js
+++ shout/backend/index.js
@@ -4,15 +4,27 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// CORS MUST BE FIRST
 app.use(cors({
   origin: 'https://joinshout.fyi',
   credentials: true
 }));
 app.options('*', cors({ origin: 'https://joinshout.fyi', credentials: true }));
+
 app.use(express.json());
+
+// Catch-all logger
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.originalUrl}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('🚨 THIS IS THE NEWEST DEPLOY 🚨');
+});
+
+app.get('/test', (req, res) => {
+  res.json({ status: 'ok' });
 });
 
 app.get('/sessions', (req, res) => {
@@ -20,7 +32,8 @@ app.get('/sessions', (req, res) => {
 });
 
 app.post('/sessions', (req, res) => {
-  // Temporary: echo back the request for testing
+  console.log('POST /sessions hit');
+  console.log('Request body:', req.body);
   res.json({ status: 'created', data: req.body });
 });
 
